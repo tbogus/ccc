5957f29084c2041a5205087ebff88c51221d8263
Paweł Charyło <pawel.charylo@radgost.com>
2024-10-27 22:35:06 +0100

    fix: pawel 6 outs

--- 6.pawel.rb
+++ 6.pawel.rb
@@ -7,10 +7,10 @@ $debug = false
 W = 3
 H = 2
 n, = scanf("%d")
-def print_mapa(debug = $debug, out = debug ? STDERR : STDOUT)
-    out.puts "-"*($mapa[0].size) if debug
-    out.puts ","*($mapa[0].size) if debug
-    $mapa[1..].each do |m|
+def print_mapa(mapa, debug = $debug, out = debug ? STDERR : STDOUT)
+    out.puts "-"*(mapa[0].size) if debug
+    out.puts ","*(mapa[0].size) if debug
+    mapa[1..].each do |m|
         if debug
             out.puts "," + m[1..].gsub(/ /, "'")
         else
@@ -26,18 +26,17 @@ n.times do
   y += 1
   ap({x: x, y: y, c: c, szerokosc: szerokosc}) if $debug
   X, Y, W = x, y, szerokosc
-  $mapa = (1..y).to_a.map { (" " * x) }
 
-  def wstaw_biurko(x, y, pionowo: true)
+  def wstaw_biurko(mapa, x, y, pionowo: true)
     STDERR.puts("wstawiam biurko w #{x},#{y} #{pionowo ? "pionowo" : "poziomo"}") if $debug
     ww = pionowo ? H : W
     hh = pionowo ? W : H
-    (0...ww).each { |xx| (0...hh).each { |yy| raise "cos jest na mapie w #{x+xx},#{y+yy} przy stawianiu biurka w #{x},#{y}" if $mapa[y+yy][x+xx] != " " }}
-    (0...ww).each { |xx| (0...hh).each { |yy| $mapa[y+yy][x+xx] = (yy==0 || xx==0) ? '.' : 'X' }}
+    (0...ww).each { |xx| (0...hh).each { |yy| raise "cos jest na mapie w #{x+xx},#{y+yy} przy stawianiu biurka w #{x},#{y}" if mapa[y+yy][x+xx] != " " }} if $debug
+    (0...ww).each { |xx| (0...hh).each { |yy| mapa[y+yy][x+xx] = (yy==0 || xx==0) ? '.' : 'X' }}
   rescue
-    print_mapa(true)
-    (0...ww).each { |xx| (0...hh).each { |yy| $mapa[y+yy][x+xx] = 'E' }}
-    print_mapa(true)
+    print_mapa(mapa, true)
+    (0...ww).each { |xx| (0...hh).each { |yy| mapa[y+yy][x+xx] = 'E' }}
+    print_mapa(mapa, true)
     raise
   end
 
@@ -50,7 +49,7 @@ n.times do
     (x/w) * (y/h)
   end
 
-  def wypelnij(x, y, xx, yy)
+  def wypelnij(mapa, x, y, xx, yy)
     pionowo = ile(xx-x, yy-y, W, H) <= ile(xx-x, yy-y, H, W)
 
     w = pionowo ? H : W
@@ -63,132 +62,141 @@ n.times do
             STDERR.puts "stawiam #{cx},#{cy}" if $debug
             # raise "wychodze za zakres X" if cx+w > xx
             # raise "wychodze za zakres Y" if cy+h > yy
-            wstaw_biurko(cx, cy, pionowo: pionowo)
+            wstaw_biurko(mapa, cx, cy, pionowo: pionowo)
             ile_jest += 1
         end
     end
     if ile_powinno_byc != ile_jest
         raise "zle liczy kwadrat #{x},#{y} #{xx},#{yy} pionowo: #{pionowo} powinno byc #{ile_powinno_byc} a jest #{ile_jest}"
     end
-
   end
 
-  wyniki = {}
-
-  (0..W*H+1).each do |cx|
-    next if cx > x
-    (0..W*H+1).each do |cy|
-        next if cy > y
-        (0...W+1).each do |dx|
-            (0...W+1).each do |dy|
-                pola = [
-                    [0, cx+dx, 0, cy],
-                    [cx+dx, x, 0, cy+dy],
-                    [cx, cx+dx, cy, cy+dy], # dziura
-                    [0, cx, cy, y],
-                    [cx, x, cy+dy, y]
-                ]
-                pola.select!{|x, xx, y, yy| xx > x && yy > y && x >= 0 && y >= 0 && xx <= X && yy <= Y}
-                wynik = pola.map{|x, xx, y, yy| ile(xx-x, yy-y) }.sum
-                wyniki[wynik] = pola
+  def iteruj(x, y)
+    (0..W*H+1).each do |cx|
+        next if cx > x
+        (0..W*H+1).each do |cy|
+            next if cy > x
+            (0...W+1).each do |dx|
+                (0...W+1).each do |dy|
+                    pola = [
+                        [0, cx+dx, 0, cy],
+                        [cx+dx, x, 0, cy+dy],
+                        [cx, cx+dx, cy, cy+dy], # dziura
+                        [0, cx, cy, y],
+                        [cx, x, cy+dy, y]
+                    ]
+                    yield pola
+                end
             end
         end
     end
-  end
 
-  (0..(W+1)*(H+1)).each do |cx|
-    next if cx > x
-    (0..(W+1)*(H+1)).each do |cy|
-#   (0..W*H+1).each do |cx|
-#     next if cx > x
-#     (0..W*H+1).each do |cy|
-        next if cy > y
-        (0...W+1).each do |dx|
-            (0...W+1).each do |dy|
-                # ((cx+dx)...x).each do |ox|
-                # # (cx+dx).step(x, W) do |ox|
-                #     pola = [
-                #         [0, cx+dx, 0, cy],
-                #         [cx+dx, ox, 0, cy+dy],
-                #         [cx, cx+dx, cy, cy+dy], # dziura
-                #         [0, cx, cy, y],
-                #         [cx, ox, cy+dy, y],
-                #         [ox, x, 0, y],
-                #     ]
-                #     pola.select!{|x, xx, y, yy| xx > x && yy > y && x >= 0 && y >= 0 && xx <= X && yy <= Y}
-                #     wynik = pola.map{|x, xx, y, yy| ile(xx-x, yy-y) }.sum
-                #     wyniki[wynik] = pola
-                # end
-                # # (cy+dy).step(y, W) do |oy|
-                # ((cy+dy)...y).each do |oy|
-                #     pola = [
-                #         [0, cx+dx, 0, cy],
-                #         [cx+dx, x, 0, cy+dy],
-                #         [cx, cx+dx, cy, cy+dy], # dziura
-                #         [0, cx, cy, oy],
-                #         [cx, x, cy+dy, oy],
-                #         [0, x, oy, y]
-                #     ]
-                #     pola.select!{|x, xx, y, yy| xx > x && yy > y && x >= 0 && y >= 0 && xx <= X && yy <= Y}
-                #     wynik = pola.map{|x, xx, y, yy| ile(xx-x, yy-y) }.sum
-                #     wyniki[wynik] = pola
-                # end
-
-                ((cx+dx)...x).each do |ox|
-                    ((cy+dy)...y).each do |oy|
+    (0..(W+1)*(H+1)).each do |cx|
+        next if cx > x
+        (0..(W+1)*(H+1)).each do |cy|
+    #   (0..W*H+1).each do |cx|
+    #     next if cx > x
+    #     (0..W*H+1).each do |cy|
+            next if cy > y
+            (0...W+1).each do |dx|
+                (0...W+1).each do |dy|
+                    ((cx+dx)...x).each do |ox|
+                    # (cx+dx).step(x, W) do |ox|
                         pola = [
                             [0, cx+dx, 0, cy],
                             [cx+dx, ox, 0, cy+dy],
                             [cx, cx+dx, cy, cy+dy], # dziura
-                            [0, cx, cy, oy],
-                            [cx, ox, cy+dy, oy],
-                            [ox, x, 0, oy],
-                            [0, x, oy, y],
+                            [0, cx, cy, y],
+                            [cx, ox, cy+dy, y],
+                            [ox, x, 0, y],
                         ]
-                        pola.select!{|x, xx, y, yy| xx > x && yy > y && x >= 0 && y >= 0 && xx <= X && yy <= Y}
-                        wynik = pola.map{|x, xx, y, yy| ile(xx-x, yy-y) }.sum
-                        wyniki[wynik] = pola
-                        # pola = [
-                        #     [0, cx+dx, 0, cy],
-                        #     [cx+dx, ox, 0, cy+dy],
-                        #     [cx, cx+dx, cy, cy+dy], # dziura
-                        #     [0, cx, cy, oy],
-                        #     [cx, ox, cy+dy, oy],
-                        #     [0, x, 0, oy],
-                        #     [ox, x, 0, y],
-                        # ]
-                        # pola.select!{|x, xx, y, yy| xx > x && yy > y && x >= 0 && y >= 0 && xx <= X && yy <= Y}
-                        # wynik = pola.map{|x, xx, y, yy| ile(xx-x, yy-y) }.sum
-                        # wyniki[wynik] = pola
+                        yield pola
                     end
+                    # (cy+dy).step(y, W) do |oy|
+                    ((cy+dy)...y).each do |oy|
+                        pola = [
+                            [0, cx+dx, 0, cy],
+                            [cx+dx, x, 0, cy+dy],
+                            [cx, cx+dx, cy, cy+dy], # dziura
+                            [0, cx, cy, oy],
+                            [cx, x, cy+dy, oy],
+                            [0, x, oy, y]
+                        ]
+                        yield pola
+                    end
+
+                    ((cx+dx)...x).each do |ox|
+                        ((cy+dy)...y).each do |oy|
+                            pola = [
+                                [0, cx+dx, 0, cy],
+                                [cx+dx, ox, 0, cy+dy],
+                                [cx, cx+dx, cy, cy+dy], # dziura
+                                [0, cx, cy, oy],
+                                [cx, ox, cy+dy, oy],
+                                [ox, x, 0, oy],
+                                [0, x, oy, y],
+                            ]
+                            yield pola
+                            # pola = [
+                            #     [0, cx+dx, 0, cy],
+                            #     [cx+dx, ox, 0, cy+dy],
+                            #     [cx, cx+dx, cy, cy+dy], # dziura
+                            #     [0, cx, cy, oy],
+                            #     [cx, ox, cy+dy, oy],
+                            #     [0, x, 0, oy],
+                            #     [ox, x, 0, y],
+                            # ]
+                            # yield pola
+                        end
+                    end
+                    # (cy+dy).step(y, W) do |oy|
+                    #     pola = [
+                    #         [0, cx+dx, 0, cy],
+                    #         [cx+dx, x, 0, cy+dy],
+                    #         [cx, cx+dx, cy, cy+dy], # dziura
+                    #         [0, cx, cy, oy],
+                    #         [cx, x, cy+dy, oy],
+                    #         [0, x, oy, y]
+                    #     ]
+                    #     pola.select!{|x, xx, y, yy| xx > x && yy > y && x >= 0 && y >= 0 && xx <= X && yy <= Y}
+                    #     wynik = pola.map{|x, xx, y, yy| ile(xx-x, yy-y) }.sum
+                    #     wyniki[wynik] = pola
+                    # end
                 end
-                # (cy+dy).step(y, W) do |oy|
-                #     pola = [
-                #         [0, cx+dx, 0, cy],
-                #         [cx+dx, x, 0, cy+dy],
-                #         [cx, cx+dx, cy, cy+dy], # dziura
-                #         [0, cx, cy, oy],
-                #         [cx, x, cy+dy, oy],
-                #         [0, x, oy, y]
-                #     ]
-                #     pola.select!{|x, xx, y, yy| xx > x && yy > y && x >= 0 && y >= 0 && xx <= X && yy <= Y}
-                #     wynik = pola.map{|x, xx, y, yy| ile(xx-x, yy-y) }.sum
-                #     wyniki[wynik] = pola
-                # end
             end
         end
     end
   end
 
-  best_wynik = wyniki.keys.max
-  pola = wyniki[best_wynik]
-  ap(best_wynik: best_wynik, pola: pola ) if $debug
+  best_wynik = 0
+  best_pola = []
 
-  pola.each do |x, xx, y, yy|
-    wypelnij(x, y, xx, yy)
+  def assert_empty_intersection(a, b) 
+    return if a[1] <= b[0] || b[1] <= a[0] || a[3] <= b[2] || b[3] <= a[2]
+    raise "niepuste przeciecie #{a} i #{b}"
   end
 
-  print_mapa
+  iteruj(x, y) do |pola| 
+    pola.select!{|x, xx, y, yy| xx > x && yy > y && x >= 0 && y >= 0 && xx <= X && yy <= Y}
+    pola.each_with_index { |a, i| pola[(i+1)..].each { |b| assert_empty_intersection(a, b) } } if !$debug
+    wynik = pola.map{|x, xx, y, yy| ile(xx-x, yy-y) }.sum
+    if wynik > best_wynik
+        best_wynik = wynik
+        best_pola = pola
+        if wynik == c
+            break 
+        end
+    end
+  end
+
+  ap(best_wynik: best_wynik, best_pola: best_pola ) if $debug
+
+  mapa = (1..y).to_a.map { (" " * x) }
+  best_pola.each do |x, xx, y, yy|
+    wypelnij(mapa, x, y, xx, yy)
+  end
+
+  print_mapa mapa
 
   if best_wynik != c
     ap({x: x, y: y, c: c, szerokosc: szerokosc})
